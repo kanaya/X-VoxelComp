2bce780e4242eb7be670de8a2150a1ab55fb1cf4
Ichi Kanaya <kanaya@pineappledesign.org>
2013-04-20 20:25:45 +0900

    Initial Commit

---
+++ VoxelComp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VoxelComp
+//
+//  Created by Ichi Kanaya on 4/20/13.
+//  Copyright (c) 2013 Pineapple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
